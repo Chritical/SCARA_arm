e2798e50ac884417fa2a804b1e78eca2467ec042
AlexanderPace <39036912+AlexanderPace@users.noreply.github.com>
2018-10-10 15:40:07 +1300

    FileCreator.java
    INPUT: ArrayList<int[]> Edges int[0] L motor, int[1] R motor
    OUTPUT: File of motor controls with format 'XXXX,XXXX,XXXX', one per line

---
+++ FileCreator.java
@@ -0,0 +1,41 @@
+/** INPUT: ArrayList<int[]> Edges int[0] L motor, int[1] R motor
+/** OUTPUT: File of motor controls with format 'XXXX,XXXX,XXXX', one per line */
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+
+public class FileCreator
+{	
+	/** Constructor. Contains test functions */
+	/*FileCreator(){
+		ArrayList<int[]> testList = new ArrayList<int[]>();
+		testList.add(new int[]{1000,2000});
+		testList.add(new int[]{3000,4000});
+		testList.add(new int[]{4000,5000});
+		
+		writeFile(testList);
+	}*/
+	
+	/** Writes the edges to the file */
+	/** Draws an edge, lifts the pen, moves to a new edge, lowers the pen, repeat */
+	public void writeFile(ArrayList<int[]> edges){
+		System.out.println("Writing file...");
+		
+		try{
+		PrintStream out = new PrintStream(new File("SCARA_instructions.txt"));
+			out.println("1500,1500,1200"); //default position, pen up
+			for (int[] line : edges){
+				out.println(line[0]+","+line[1]+",1200"); //pen up
+				out.println(line[0]+","+line[1]+",1800"); //pen down
+			}
+		}
+		catch (IOException e){ Trace.println("FAIL: "+e); }
+		
+		System.out.println("Success \nFile written to \"SCARA_instructions\"");
+	}
+	
+	/*public static void main(String[] args){
+		FileCreator fileCreator = new FileCreator();
+	}*/
+}
