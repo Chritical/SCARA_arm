9b994ccf487b41cb97fa124429db2db4b548e892
AlexanderPace <39036912+AlexanderPace@users.noreply.github.com>
2018-10-10 16:00:00 +1300

    SCARAImageCreator.java
    The main file that makes everything do the things.

---
+++ SCARAImageCreator.java
@@ -0,0 +1,49 @@
+/** Takes an image and converts it to code that can be submitted to the SCARA
+ * robot arm for drawing
+ * 
+ * ENGR110, 2018, T2
+ * Team baconCow();
+ * 
+ * Team members:
+ * Christopher Meekin
+ * Sam Kain
+ * Riley Allen
+ * Alexander Pace
+ */
+
+import ecs100.*;
+import java.util.*;
+import java.awt.Color;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+public class SCARAImageCreator
+{
+	SCARAImageCreator(){
+		//currently all classes and methods are preliminary, may need to be changed to fit actual files
+		ImageLoader imageLoader = new ImageLoader();
+		ShapeProcessor shapeProcessor = new ShapeProcessor();
+		EdgeProcessor edgeProcessor = new EdgeProcessor();
+		CoordinateProcessor coordinateProcessor = new CoordinateProcessor();
+		FileCreator fileCreator = new FileCreator();
+		
+		System.out.println("Welcome to team baconCow();'s image creator for the SCARA robot arm");
+		System.out.println("");
+		System.out.println("Reading in file...");
+		imageLoader.loadImage();
+		System.out.println("Processing file...");
+		shapeProcessor.processShape();
+		System.out.println("Finding edges...");
+		edgeProcessor.findEdges();
+		System.out.println("Converting to coordinates...");
+		coordinateProcessor.processCoordinates();
+		System.out.println("Saving to file...");
+		fileCreator.writeFile();
+	}
+	
+	public static void main (String[] args)}{
+		SCARAImageCreator imageCreator = new SCARAImageCreator();
+	}
+	
+}
