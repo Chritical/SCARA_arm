24eec4cfec5d59b4221c4c7dce17edc8bf9568f5
AlexanderPace <39036912+AlexanderPace@users.noreply.github.com>
2018-10-16 14:29:42 +1300

    Updated to work with correct input
    Now uses ArrayList<ArrayList<int[]>> as input.

--- FileCreator.java
+++ FileCreator.java
@@ -1,4 +1,4 @@
-/** INPUT: ArrayList<int[]> Edges int[0] L motor, int[1] R motor
+/** INPUT: ArrayList<ArrayList<int[]>> coordinates, int[0] L motor, int[1] R motor
 /** OUTPUT: File of motor controls with format 'XXXX,XXXX,XXXX', one per line */
 
 import ecs100.*;
@@ -24,7 +24,53 @@ public class FileCreator
 		System.out.println("Writing file...");
 		
 		try{
+		PrintStream out = new PrintStream(new Fi/** INPUT: ArrayList<int[]> Edges int[0] L motor, int[1] R motor
+/** OUTPUT: File of motor controls with format 'XXXX,XXXX,XXXX', one per line */
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+
+public class FileCreator
+{	
+	ArrayList<ArrayList<int[]>> coords;
+	
+	/** Constructor */
+	FileCreator(ArrayList<ArrayList<int[]>> coordinates){
+		//Test array (currently invalid, update to ArrayList<ArrayList<int[]>> to test
+		/*ArrayList<int[]> testList = new ArrayList<int[]>();
+		testList.add(new int[]{1000,2000});
+		testList.add(new int[]{3000,4000});
+		testList.add(new int[]{4000,5000});*/
+		
+		coords = coordinates;
+	}
+	
+	/** Writes the coords to the file */
+	/** Draws an edge, lifts the pen, moves to a new edge, lowers the pen, repeat */
+	public void writeFile(){
+		//System.out.println("Writing file...");
+		
+		try{
 		PrintStream out = new PrintStream(new File("SCARA_instructions.txt"));
+			out.println("1500,1500,1200"); //default position, pen up
+			for (ArrayList<int[]> coordSet : coords){
+				for (int[] line : coordSet){
+					out.println(line[0]+","+line[1]+",1200"); //pen up
+					out.println(line[0]+","+line[1]+",1800"); //pen down
+				}
+			}
+		}
+		catch (IOException e){ Trace.println("FAIL: "+e); }
+		
+		System.out.println("Success, file written to \"SCARA_instructions\"");
+	}
+	
+	/*public static void main(String[] args){
+		FileCreator fileCreator = new FileCreator();
+	}*/
+}
+le("SCARA_instructions.txt"));
 			out.println("1500,1500,1200"); //default position, pen up
 			for (int[] line : edges){
 				out.println(line[0]+","+line[1]+",1200"); //pen up
