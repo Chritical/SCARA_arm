d2db73392bf68080f103595b26471d83a48e50ae
AlexanderPace <39036912+AlexanderPace@users.noreply.github.com>
2018-10-16 13:54:56 +1300

    Compatability changes

--- EdgeProcessor.java
+++ EdgeProcessor.java
@@ -16,17 +16,18 @@ class EdgeProcessor{
 	int[][] imgV;
 	int[][] imgH;
 	
-	public EdgeProcessor(){
-        int x[][] = {{250,250,250,250,50,50,50,50,50},
+	public EdgeProcessor(int input[][]){
+		//Test case
+        	/*int input[][] = {{250,250,250,250,50,50,50,50,50},
 					{250,250,250,250,50,50,50,50,50},
 					{250,250,250,250,50,50,50,50,50},
 					{250,250,250,250,50,50,50,50,50},
 					{50,50,50,50,50,50,50,50,50},
-					{50,50,50,50,50,50,50,50,50}};
-        edgeProcessor(x);
+					{50,50,50,50,50,50,50,50,50}};*/
+        processEdges(input);
     }
     
-	public int[][] edgeProcessor(int img[][]){
+	public int[][] processEdges(int img[][]){
 		
 		
 		brd = new int[img.length+2][img[0].length+2];
@@ -38,39 +39,39 @@ class EdgeProcessor{
 		
 		//convolutes the image into its edges
 		for(int i=1;i<img.length-1; i++){
-				for(int j=1; j<img[0].length-1;j++){
-					
-					imgV[i][j] = img[i-1][j-1]*vPro[0][0]+img[i][j-1]*vPro[1][0]+img[i+1][j-1]*vPro[2][0]+
-								 img[i-1][j]  *vPro[0][1]+img[i][j]  *vPro[1][1]+img[i+1][j]  *vPro[2][1]+
-								 img[i-1][j+1]*vPro[0][2]+img[i][j+1]*vPro[1][2]+img[i+1][j+1]*vPro[2][2];
-					
-					imgH[i][j] = img[i-1][j-1]*hPro[0][0]+img[i][j-1]*hPro[1][0]+img[i+1][j-1]*hPro[2][0]+
-							     img[i-1][j]  *hPro[0][1]+img[i][j]  *hPro[1][1]+img[i+1][j]  *hPro[2][1]+
-								 img[i-1][j+1]*hPro[0][2]+img[i][j+1]*hPro[1][2]+img[i+1][j+1]*hPro[2][2];
-				}
+			for(int j=1; j<img[0].length-1;j++){
+
+				imgV[i][j] = img[i-1][j-1]*vPro[0][0]+img[i][j-1]*vPro[1][0]+img[i+1][j-1]*vPro[2][0]+
+					img[i-1][j]  *vPro[0][1]+img[i][j]  *vPro[1][1]+img[i+1][j]  *vPro[2][1]+
+					img[i-1][j+1]*vPro[0][2]+img[i][j+1]*vPro[1][2]+img[i+1][j+1]*vPro[2][2];
+
+				imgH[i][j] = img[i-1][j-1]*hPro[0][0]+img[i][j-1]*hPro[1][0]+img[i+1][j-1]*hPro[2][0]+
+				     	img[i-1][j]  *hPro[0][1]+img[i][j]  *hPro[1][1]+img[i+1][j]  *hPro[2][1]+
+					img[i-1][j+1]*hPro[0][2]+img[i][j+1]*hPro[1][2]+img[i+1][j+1]*hPro[2][2];
+			}
 				
 		}
 		
-		//creates a borderd version of the edged image
+		//creates a bordered version of the edged image
 		for(int i=0; i<brd[0].length; i++){
-				brd[0][i]=0;
-				brd[brd.length-1][i]=0;
+			brd[0][i]=0;
+			brd[brd.length-1][i]=0;
 		}
 		for(int i=0; i<brd.length; i++){
-				brd[i][0]=0;
-				brd[i][brd[0].length-1]=0;
+			brd[i][0]=0;
+			brd[i][brd[0].length-1]=0;
 		}
 		for(int i=0;i<img.length; i++){
-				for(int j=0; j<img[0].length;j++){
-						brd[i+1][j+1]=(int)(Math.sqrt(imgH[i][j]*imgH[i][j] +
-									      imgV[i][j]*imgV[i][j]) * 255 / Math.sqrt((1020^2)*2));
-						
-				}
+			for(int j=0; j<img[0].length;j++){
+				brd[i+1][j+1]=(int)(Math.sqrt(imgH[i][j]*imgH[i][j] +
+						      imgV[i][j]*imgV[i][j]) * 255 / Math.sqrt((1020^2)*2));
+
+			}
 		}
 		return brd;
 	}
 	
-	public static void main(String[] args){
+	/*public static void main(String[] args){
 			EdgeProcessor im = new EdgeProcessor();
-	}
+	}*/
 }
